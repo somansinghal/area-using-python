31ef106e28135a8ac89de6e6a1ba72a2812020a5
Soman Singhal <somansinghal06@gmail.com>
2025-12-31 08:15:42 +0530

    Add area calculations for various shapes

---
+++ main.py
@@ -0,0 +1,30 @@
+breath=float(input('enter the breadth = '))
+length=float(input('enter the length = '))
+area=(breath*length)
+print(area,"area of rectangle")
+
+radius=float(input('enter the radius = '))
+pie=3.14
+a1=(pie*radius*radius)
+print(a1,"area of circle")
+
+side=float(input('enter the side = '))
+a2=(side*side)
+print(a2,"area of square")
+
+b=float(input('enter the base = '))
+h=float(input('enter the height = '))
+a3=(0.5*b*h)
+print(a3,"area of triangle")
+
+l=float(input('enter the length = '))
+s=float(input('enter the side length = '))
+c=float(input('enter the c = '))
+a4=(s*l*c)
+print(a4,"area of cube")
+
+l2=float(input('enter the length = '))
+w=float(input('enter the width = '))
+h=float(input('enter the heigth ='))
+a5=(h*w*l)
+print(a5,"area of cuboid")
